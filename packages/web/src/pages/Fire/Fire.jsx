faaa7085f9b8ee105f3e577eda263f057ff3a011
frontendmonster <frontendmonster@gmail.com>
2018-10-11 04:09:47 +0330

    Add fire particles

--- packages/web/src/pages/Fire/Fire.jsx
+++ packages/web/src/pages/Fire/Fire.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import Particles from 'react-particles-js';
 import styles from './Fire.scss';
 import { Section } from '../../components';
 
@@ -8,7 +9,48 @@ const cx = classNames.bind(styles);
 
 
 const Fire = () => (
-  <Section className={cx('root')} />
+  <Section className={cx('root')}>
+    <Particles
+      className={cx('particles')}
+      params={{
+        particles: {
+          number: {
+            value: 120,
+            density: {
+              enable: false,
+            },
+          },
+          opacity: {
+            value: 0.3,
+            random: true,
+            anim: { enable: true, opacity_min: 0.1 },
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              speed: 8,
+              size_min: 0.3,
+            },
+          },
+          shape: {
+            type: 'triangle',
+          },
+          line_linked: {
+            enable: false,
+          },
+          move: {
+            random: true,
+            speed: 1.5,
+            direction: 'top',
+            out_mode: 'out',
+          },
+        },
+      }}
+    />
+
+  </Section>
+
 );
 
 export default Fire;
